b22beb7c85cabe8365d2b7aa6043eaf1f12cf641
srinivasan <srinivasan.r@emdsys.com>
2018-05-18 18:27:37 +0530

    socket io included

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ i18n.configure({
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var chat = require('./routes/chat');
 var ri18n = require('./routes/i18n');
 
 var app = express();
@@ -22,6 +23,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use('/nm', express.static(__dirname + '/node_modules/'));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -37,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/i18n', ri18n);
+app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
